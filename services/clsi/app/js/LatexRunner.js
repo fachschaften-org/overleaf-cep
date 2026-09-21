ac438bc1ac4c158866118ca4c7ab96217793cbbc
MonsterDruide1 <5958456@gmail.com>
2026-09-21 14:01:16 +0200

    enable shellescape

--- services/clsi/app/js/LatexRunner.js
+++ services/clsi/app/js/LatexRunner.js
@@ -180,6 +180,7 @@ function _buildLatexCommand(mainFile, opts = {}) {
     '-auxdir=$COMPILE_DIR',
     '-outdir=$COMPILE_DIR',
     '-synctex=1',
+    '-shell-escape',
     '-interaction=batchmode',
     '-time'
   )
